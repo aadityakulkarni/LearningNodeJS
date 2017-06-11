08af7384e3d223a07cd2333ff1fbf14987c01868
aadikulkarni91@gmail.com <aadikulkarni91@gmail.com>
2017-06-11 14:53:36 -0500

    minor changes

--- chatApp/server/server.js
+++ chatApp/server/server.js
@@ -1,5 +1,14 @@
 const path = require('path');
+const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 
-console.log(publicPath);
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log(`Server up and running on port 3000`);
+});
+
+// console.log(publicPath);
